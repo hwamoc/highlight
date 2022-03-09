266428528d668f1e7be2ce7f72505a960f74bed4
hwamoc <hmkim3712@gmail.com>
2022-03-10 00:08:33 +0900

    docs: highlight 데이터 생성 수정 (color ordinal 값 1 -> 서수)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,25 +32,25 @@ VALUES (5, 3, 'www.earticle.net/Article/A225635');
 
 -- Highlight 데이터 생성
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (1, 1, 1, 1, '처음 긋는 하이라이트~~', '2021-01-24 12:10:30');
+VALUES (1, 1, 1, 'first', '처음 긋는 하이라이트~~', '2021-01-24 12:10:30');
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (2, 1, 1, 1, '구우우웃 구우우웃', null);
+VALUES (2, 1, 1, 'first', '구우우웃 구우우웃', null);
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (3, 1, 1, 2, '꾸르잼잼잼 ^0^', '2021-03-28 14:20:30');
+VALUES (3, 1, 1, 'second', '꾸르잼잼잼 ^0^', '2021-03-28 14:20:30');
 
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (4, 2, 2, 1, 'AROUND', null);
+VALUES (4, 2, 2, 'first', 'AROUND', null);
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (5, 2, 2, 2, '디자인', '2021-07-30 12:30:20');
+VALUES (5, 2, 2, 'second', '디자인', '2021-07-30 12:30:20');
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (6, 2, 3, 3, 'SPACE', '2021-10-21 16:40:40');
+VALUES (6, 2, 3, 'third', 'SPACE', '2021-10-21 16:40:40');
 
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (7, 3, 4, 1, '뚜두뚜두뚜', '2021-01-25 12:10:30');
+VALUES (7, 3, 4, 'first', '뚜두뚜두뚜', '2022-01-25 12:10:30');
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (8, 3, 4, 2, 'blank pink in your area', '2021-01-24 10:30:10');
+VALUES (8, 3, 4, 'second', 'blank pink in your area', '2022-01-24 10:30:10');
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (9, 3, 5, 1, 'everything i need is on the ground', '2021-03-24 18:30:00');
+VALUES (9, 3, 5, 'first', 'everything i need is on the ground', '2022-03-24 18:30:00');
 INSERT INTO highlight(id, user_id, page_id, color_ordinal, text, modified_date)
-VALUES (10, 3, 5, 3, 'lets kill this love', null);
+VALUES (10, 3, 5, 'third', 'lets kill this love', null);
 
